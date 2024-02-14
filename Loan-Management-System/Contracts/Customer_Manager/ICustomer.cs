da1f9cb98f0aff9add0537d0518635247cd0ba05
Lonwabo Nomyayi <lonwabonomyayi@gmail.com>
2024-02-14 21:19:30 +0200

    Add repo class and contract for customer transactions

---
+++ Loan-Management-System/Contracts/Customer_Manager/ICustomer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loan_Management_System.Contracts.Customer_Manager
+{
+    public interface ICustomer
+    {
+    }
+}
---
+++ Loan-Management-System/Repositories/Customer_Manager/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Loan_Management_System.Repositories.Customer_Manager
+{
+    public class Customer : ICustomer 
+    {
+
+    }
+}
