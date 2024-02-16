0bb3f7643b1da6b7111279b96ca0109df5489882
Lonwabo Nomyayi <lonwabonomyayi@gmail.com>
2024-02-16 18:31:14 +0200

    Add Sign in page, AccountController, repository method for getting user information

---
+++ Loan-Management-System/Loan-Management-System/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Loan_Management_System.Models
+{
+    public class Customer
+    {
+        public int CustomerKey { get; set; }
+        public String CustomerName { get; set; }
+        public String CustomerIDNumber { get; set; }
+        public int CustomerPayDay { get; set; }
+        public double CustomerSalary { get; set; }
+        public String CustomerAddress1 { get; set; }
+        public String CustomerAddress2 { get; set; }
+        public String CustomerAddress3 { get; set; }
+        public String CustomerAddress4 { get; set; }
+        public String CustomerPostalCde { get; set; }
+    }
+}
