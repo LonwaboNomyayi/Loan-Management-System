b66b5800438c5a36cbfe869bd0d3278818958457
Lonwabo Nomyayi <lonwabonomyayi@gmail.com>
2024-02-16 18:31:14 +0200

    Add Sign in page, AccountController, repository method for getting user information

---
+++ Loan-Management-System/Loan-Management-System/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using DataAccessLayer.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loan_Management_System.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccounts account = new Accounts();
+        // GET: Account
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult Signin(SignInDetails model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDetails = account.GetUserDetails(model);
+                if(userDetails != null)
+                {
+                    //then call the SessionHelper to store the userdetails - as we may need this information in some parts of the system 
+                    return Json(new { data = true }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(new { data = false }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
