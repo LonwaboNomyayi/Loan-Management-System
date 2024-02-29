269d5f5834aa031e5ac357fe68f6027e68d488a3
Lonwabo Nomyayi <lonwabonomyayi@gmail.com>
2024-02-29 14:35:18 +0200

    Add jquery ajax call to load the data bind to the dataTable

---
+++ Loan-Management-System/Loan-Management-System/Scripts/Application Scripts/Loan/Loans.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+
+    LoadLoans()
+});
+
+function LoadLoans() {
+    $('#tblCustomers').DataTable({
+        "ajax": {
+            "type": 'GET',
+            "url": '/Loan/GetAllLoansForStore',
+            "data": '{}',
+            "contentType": 'application/json; charset=utf-8',
+            "datatype": 'json',
+            "cache": false,
+        },
+        "columns": [
+            { "data": "LoanID", "visible": false, "searchable": false },
+            { "data": "LoanDate" },
+            { "data": "LoanHolder" },
+            { "data": "LoanAmount" },
+            { "data": "ReturnDate" },
+            { "data": "InterestCharged" },
+            {
+                "data": "LoanID",
+                "render": function (data, type, full) {
+                    if (data != "") {
+                        var customerId = data;
+                        return "<a class='btn btn-warning btn-sm' onclick=navigateToLoanDetails('" + data + "')><span class ='ti-pencil-alt'></span> Edit</a>";
+                    }
+                }
+            }
+        ],
+        "scrollY": "520px",
+        "scrollCollapse": true
+    });
+}
